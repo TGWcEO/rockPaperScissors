916c234ce8190bc618b573b0fb4d035260102b1d
Wrighteous <AriAuto@yahoo.com>
2023-11-15 23:48:06 -0500

    created a way for the user interface to be reset at the end of each game, added code to prevent the gam from going further than 5 points, now it gives the player another button to use to reset the game when its over, if the player instead presses one of the game buttons (rock,papper,scissor) the game resets automatically. I have tried it out several times and I think its ready to be merged with the main branch

--- javascript.js
+++ javascript.js
@@ -13,10 +13,19 @@ const box=document.querySelector('#playBox');
 const scoreboard=document.createElement('div');
 const choices=document.createElement('div');
 const announce=document.createElement('div');
-start.addEventListener('click', createPlay);
+start.addEventListener('click',createPlay);
 
 function createPlay(){
   //find out how to remove all children and stick it here
+  //while box.hasChild
+  //remove first child
+  let child=box.lastElementChild;
+  while(child){
+    box.removeChild(child);
+    child=box.lastElementChild;
+  }
+  userC="";
+  compC="";
   userPoints=0;
   compPoints=0;
   const rock=document.createElement('button');
@@ -25,34 +34,53 @@ function createPlay(){
   rock.textContent='ROCK';
   paper.textContent='PAPER';
   scissor.textContent='SCISSOR';
+  score();
+  announce.textContent="Ready to Start a New Game!";
   box.appendChild(scoreboard);
   box.appendChild(rock);
   box.appendChild(paper);
   box.appendChild(scissor);
   box.appendChild(choices);
   box.appendChild(announce);
-  rock.addEventListener('click',()=>{userChoice='r';whoWon();});
-  paper.addEventListener('click',()=>{userChoice='p';whoWon();});
-  scissor.addEventListener('click',()=>{userChoice='s';whoWon();});
+  rock.addEventListener('click',()=>{userChoice='r';userC="Rock";whoWon();});
+  paper.addEventListener('click',()=>{userChoice='p';userC="Paper";whoWon();});
+  scissor.addEventListener('click',()=>{userChoice='s';userC="Scissors";whoWon();});
 }
 
-  scoreboard.textContent="Your Score: "+userPoints+"\nComputerScore: "+compPoints;
+function startNew(){
+  const start2=document.createElement('button')
+  start2.textContent="Start A New Game";
+  start2.addEventListener("click",createPlay)
+  box.appendChild(start2);
+}
 
 function play(){
   if(userPoints<5&&compPoints<5){
-    scoreboard.textContent="Your Score: "+userPoints+"\nComputer Score: "+compPoints;
+    score();
   }else if(userPoints==5){
+    score();
     announce.textContent="YAY!! You Won The Game!!";
-//  playAgain();
-  }else{
+    startNew();
+    userPoints=userPoints+10;
+    compPoints=compPoints+10;
+  }else if(compPoints==5){
+    score()
     announce.textContent="SORRY. You Lost The Game.";
-//  playAgain();
+    startNew();
+    userPoints=userPoints+10;
+    compPoints=compPoints+10;
+  }else{
+    createPlay()
   }
+}
+
+function score(){
+  scoreboard.textContent="Your Score: "+userPoints+" / / Computer Score: "+compPoints;
+  choices.textContent="You Choose: "+userC+" / Comp Choose: "+compC;
 }
 
 function compChoose(){
   let randomNum=Math.floor(Math.random()*(3-1+1)+1);
-//console.log("Random Number: ",randomNum)
   if (randomNum==1){
     compC='Rock';
     compChoice="r";
@@ -80,7 +108,6 @@ function tie(){
 function whoWon(){
   compChoose();
   if (userChoice=="r"){
-    userC="Rock";
     if (compChoice=="r"){;
       tie();
     }else if(compChoice=="p"){
@@ -89,7 +116,6 @@ function whoWon(){
       win();
     }
   }else if(userChoice=="p"){
-    userC="Paper";
     if(compChoice=="r"){
       win();
     }else if(compChoice=="p"){
@@ -98,7 +124,6 @@ function whoWon(){
       lose();
     }
   }else{
-    userC="Scissors";
     if(compChoice=="r"){
       lose();
     }else if(compChoice=="p"){
@@ -107,7 +132,6 @@ function whoWon(){
       tie();
     }
   }
-  choices.textContent="You: "+userC+" / / Computer: "+compC;
   play();
 }
 
